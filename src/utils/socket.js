ed4f0a4d7cc5484459276a7229a3fe804073b251
lenin12345-dev <leninmohapatra@gmail.com>
2025-07-17 20:10:30 -0400

    socket chat add

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,4 +1,9 @@
 const socket = require("socket.io");
+const crypto = require("crypto")
+
+const getSecretRoomId = (userId,targetUserId)=>{
+    return crypto.createHash("sha256").update([userId,targetUserId].sort().join("_")).digest("hex")
+}
 
 const initializeSocket = (server) => {
   const io = socket(server, {
@@ -8,15 +13,17 @@ const initializeSocket = (server) => {
     },
   });
   io.on("connection", (socket) => {
-    socket.on("joinChat",()=>{
-
+    socket.on("joinChat",({userId,targetUserId})=>{
+         const room = getSecretRoomId(userId,targetUserId)
+         socket.join(room)
     })
-    socket.on("sendMessage",()=>{
-
+    socket.on("sendMessage",({firstName,userId,targetUserId,text})=>{
+      const room = getSecretRoomId(userId,targetUserId) 
+        io.to(room).emit("messageReceived",{firstName,text})
     })
     socket.on("disconnect",()=>{
 
     })
   });
 };
-export default initializeSocket
+module.exports = initializeSocket;
